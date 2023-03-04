3a422b2f68213b17b9dd7583c6ba62d1fbe7f480
Cédric Paimblanc <cedric.du.50@gmail.com>
2023-03-04 01:20:39 +0100

    Ajout de ressource

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+const swaggerJsdoc = require('swagger-jsdoc');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const { config } = require('dotenv');
 config();
 const PORT = process.env.PORT || 3000;
-// const PORT = 4000 //parseInt(process.env.PORT) || 3000;
-
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./doc-api.json");
 app.use(cors());
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
+//create a swagger (Documentation)
 require("./routes/game.routes")(app);
 require("./routes/ressource.routes")(app);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
 	console.log('Server is running on port ' + PORT
 	)
